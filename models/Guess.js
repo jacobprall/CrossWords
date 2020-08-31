4319a1ea3f240c8845d352d736e3967cd493314f
Phil Gresham <philgresham@gmail.com>
2020-08-31 15:33:35 -0700

    Add guess model and validation. Minor changes to User/Word model

---
+++ models/Guess.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Word = require('./Word');
+
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
+
+const GuessSchema = new Schema(
+  {
+    gameId: {
+      type: ObjectId,
+      required: true,
+    },
+    guessIndex: {
+      type: Number,
+      required: true,
+    },
+    correct: {
+      type: Boolean,
+      require: true,
+    },
+    word: {
+      type: Word,
+      required: true,
+      uppercase: true,
+    },
+    position: {
+      type: [Number],
+      required: true,
+    },
+    points: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Guess = mongoose.model('Guess', GuessSchema);
+module.exports = Guess;
--- models/User.js
+++ models/User.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true
-});
+const { Schema } = mongoose;
 
-module.exports = User = mongoose.model('User', UserSchema);
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
