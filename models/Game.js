ceb874ac7e076769d1ea65503c9a4b3ff1e655fd
Phil Gresham <philgresham@gmail.com>
2020-08-31 12:09:25 -0700

    Add Game model, minor change to User model

---
+++ models/Game.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Word = require('./Word');
+
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
+
+const GameSchema = new Schema(
+  {
+    user: {
+      type: ObjectId,
+      required: true,
+    },
+    wordsSent: {
+      type: Array,
+      required: true,
+    },
+    wordsGuessed: {
+      type: Array,
+    },
+    score: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Game = mongoose.model('Game', GameSchema);
+module.exports = Game;
