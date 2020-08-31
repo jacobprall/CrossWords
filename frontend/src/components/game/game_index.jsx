c2bd563eae8b4683ad51150313a9614004176daa
Jim-Heftypants <nickcsercel@gmail.com>
2020-08-31 15:04:17 -0700

    initial commit game utils

---
+++ frontend/src/components/game/game_index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GameItem from './game_item';
+
+class GameIndex extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default GameIndex;
---
+++ frontend/src/components/game/game_item.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class GameItem extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default GameItem;
