034ac89763e276e884ff6d1090e0b85c9bfdb6a7
Aatef Baransy <baransymusic@gmail.com>
2020-08-31 15:12:38 -0700

    last push didn't work

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -3,9 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 import { NavLink } from 'react-router-dom';
 import { clearSessionErrors } from '../../actions/session_actions';
-import logo from '../../images/logo.png'; 
-import styled from 'styled-components'; 
-
+import logo from '../../images/logo.png'
+import styled from 'styled-components';
 
 const NavContainer = styled.div`
   display: flex; 
@@ -14,7 +13,7 @@ const NavContainer = styled.div`
   padding-right: 1rem; 
   padding-top: 0.5rem; 
   padding-bottom: 0.5rem; 
-  background-color:	#E8E8E8;
+  background-color: #E8E8E8;
   border-bottom: 0.05rem solid #A0A0A0; 
   box-shadow: 0px 0px 5px #D3D3D3;
   position: ${props => props.stick ? "fixed;" : ";"}
@@ -31,10 +30,36 @@ const LinksContainer = styled.div`
 `
 
 const LoggedInNavigationSection = styled.div`
+  width: 30rem; 
   display: flex; 
+  justify-content: space-around; 
+  align-items: center; 
 `
 
-const LoggedInNavLink = styled.div`
+const LoggedInNavLink = styled(NavLink)`
+    padding: 0.5rem 0.5rem 0rem 0.5rem; 
+    height: 1.7rem; 
+    width: 6rem;
+    font-weight: 600; 
+    text-align: center; 
+    background-color: #A8A8A8; 
+    border-radius: 0.2rem; 
+    &:hover {
+      background-color: #696969; 
+    cursor: pointer; 
+  }
+`
+
+const LogoutButton = styled.div`
+  padding: 0.5rem 0.5rem 0.5rem 0.5rem; 
+    width: 4rem;
+    font-weight: 600; 
+    text-align: center; 
+    background-color: #A8A8A8; 
+    border-radius: 1rem; 
+    &:hover {
+      background-color: #696969; 
+    cursor: pointer; 
 `
 
 const Links = styled.div`
@@ -62,7 +87,7 @@ const Logo = styled.img`
   height: 2rem; 
 `
 
-export default function Navbar({stick, ele}) {
+export default function Navbar({ stick, ele }) {
   const dispatch = useDispatch();
   const loggedIn = useSelector((state) => state.session.isAuthenticated);
   const logoutUser = (e) => {
@@ -73,42 +98,41 @@ export default function Navbar({stick, ele}) {
     dispatch(clearSessionErrors());
   };
   const getLinks = () => {
-    let links; 
+    let links;
     if (loggedIn) {
-        links = (
-          <LoggedInNavigationSection>
-            <Links className="nav-left">
-              <LoggedInNavLink to={"/"}>Home</LoggedInNavLink>
-              <LoggedInNavLink to={"/"}>New Game</LoggedInNavLink>
-              <LoggedInNavLink to={"/"}>Stats</LoggedInNavLink>
-            </Links>
-            <LoggedInNavLink className="nav-right" onClick={logoutUser}>Logout</LoggedInNavLink>
-          </LoggedInNavigationSection>
-        )
+      links = (
+        <LoggedInNavigationSection>
+          {/* <Links className="nav-left"> */}
+          <LoggedInNavLink to={"/"}>Home</LoggedInNavLink>
+          <LoggedInNavLink to={"/"}>New Game</LoggedInNavLink>
+          <LoggedInNavLink to={"/"}>Stats</LoggedInNavLink>
+          {/* </Links> */}
+          <LogoutButton className="nav-right" onClick={logoutUser}>Logout</LogoutButton>
+        </LoggedInNavigationSection>
+      )
     } else {
-        links = (
-          <Links>
-            <SignUpAndLogin to={"/signup"} onClick={handleAlt}>Sign Up</SignUpAndLogin>
-            <SignUpAndLogin to={"/login"} onClick={handleAlt}>Log In</SignUpAndLogin>
-          </Links>
-        )
-      
+      links = (
+        <Links>
+          <SignUpAndLogin to={"/signup"} onClick={handleAlt}>Sign Up</SignUpAndLogin>
+          <SignUpAndLogin to={"/login"} onClick={handleAlt}>Log In</SignUpAndLogin>
+        </Links>
+      )
+
     }
 
-    return links; 
+    return links;
   }
 
   return (
     <NavContainer ref={ele} stick={stick}>
       <LogoAndLinks>
-        <Logo src={logo}></Logo>
+        <NavLink to={"/"}>
+          <Logo src={logo}></Logo>
+        </NavLink>
         <LinksContainer>
-          {getLinks()} 
+          {getLinks()}
         </LinksContainer>
       </LogoAndLinks>
     </NavContainer>
   )
 }
-
-
-
