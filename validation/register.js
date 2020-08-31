eecac8632c2befbb696e8841cf8871285abaaabb
Phil Gresham <philgresham@gmail.com>
2020-08-31 12:22:23 -0700

    Prettify validations. No other changes.

--- validation/register.js
+++ validation/register.js
@@ -1,18 +1,20 @@
 const Validator = require('validator');
 const validText = require('./valid-text');
 
-module.exports = function validateRegisterInput(data) {
-  let errors = {};
+module.exports = function validateRegisterInput(oldData) {
+  const data = { ...oldData };
+  const errors = {};
 
   data.username = validText(data.username) ? data.username : '';
   data.email = validText(data.email) ? data.email : '';
   data.password = validText(data.password) ? data.password : '';
 
-
-  if (!Validator.isLength(data.username, {
+  if (
+    !Validator.isLength(data.username, {
       min: 2,
-      max: 30
-    })) {
+      max: 30,
+    })
+  ) {
     errors.username = 'Username must be between 2 and 30 characters';
   }
 
@@ -32,16 +34,17 @@ module.exports = function validateRegisterInput(data) {
     errors.password = 'Password field is required';
   }
 
-  if (!Validator.isLength(data.password, {
+  if (
+    !Validator.isLength(data.password, {
       min: 6,
-      max: 30
-    })) {
+      max: 30,
+    })
+  ) {
     errors.password = 'Password must be at least 6 characters';
   }
 
-
   return {
     errors,
-    isValid: Object.keys(errors).length === 0
+    isValid: Object.keys(errors).length === 0,
   };
 };
