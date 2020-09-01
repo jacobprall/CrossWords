3270f431165247ca37e3485e13bfa2cd80577270
Jacob <prall.jacob@gmail.com>
2020-09-01 12:51:36 -0700

    heroku

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
@@ -18,3 +19,11 @@ app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api/users', users);
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
