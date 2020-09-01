61d2720740c4640d4a40a11047b48d04d0492518
Jacob <prall.jacob@gmail.com>
2020-09-01 08:17:05 -0700

    add game post and patch routes

---
+++ routes/api/games.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Game = require('../../models/Game');
+// const getFirstWord method required to fetch first word from database
+// getNextWord
+
+
+// Frontend sends userId in post request
+router.post('/', (req, res) => {
+  //is this the route we want?
+  const userId = req.userId;
+  const newGame = new Game({
+    user: userId,
+    wordsSent: [], // send result of getFirstWord
+    wordsGuessed: [],
+    score: 0,
+  });
+  newGame.save().then((game) => res.json(game));
+});
+
+// User sends score, words guessed, (timer?)
+// needs getNextClue method
+router.patch('/', (req, res) => {
+  Game.findById(req.params.id)
+  .then((game) => {
+    game.score = req.score;
+    game.wordsGuessed = req.wordsGuessed;
+    game.wordsSent = req.wordsSent.push() // getNextWord method
+    return res.json(game)
+  });
+});
+
+module.exports = router;
---
+++ routes/api/words.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Word = require('../../models/Word')
