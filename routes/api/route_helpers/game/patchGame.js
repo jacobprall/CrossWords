5910bf1d5a381bcc05cddfd73cf46a76268e9534
Jacob <prall.jacob@gmail.com>
2020-09-01 18:59:24 -0700

    added nextword, nextindex, nextdir to getNextWord return, and newGame and patchGame functions

--- routes/api/route_helpers/game/patchGame.js
+++ routes/api/route_helpers/game/patchGame.js
@@ -1,14 +1,24 @@
 const Game = require('../../../../models/Game');
 
 module.exports = (req, res) => {
+   
   Game.findById(req.params.id)
     .then((game) => {
+      //added
+      const nextWordResult = await getNextWord(game.wordsGuessed, game.maxLength, game.dir)[0];
+      const nextWord = nextWordResult[0];
+      const nextIndex = nextWordResult[1];
+      const nextDir = nextWordResult[2];
+
       const newGame = { ...game };
       newGame.score = req.score;
       newGame.wordsGuessed = req.wordsGuessed;
       newGame.maxlength = req.maxLength;
       newGame.dir = req.dir;
-      newGame.wordsSent = req.wordsSent.push(); // getNextWord(game.wordsGuessed, game.len, game.dir)
+      newGame.wordsSent = req.wordsSent.push(nextWord); // getNextWord(game.wordsGuessed, game.len, game.dir)
+      //added
+      newGame.nextIndex = nextIndex;
+      newGame.nextDir = nextDir;
       return newGame;
     })
     .then((game) => res.json(game));
