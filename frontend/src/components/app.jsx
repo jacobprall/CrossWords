d4766e4754018246f9e2608613f6537c3b5c5bf1
Aatef Baransy <baransymusic@gmail.com>
2020-09-02 16:57:16 -0700

    grid state persists across rerenders

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch, useHistory } from "react-router-dom";
 import { useSelector } from 'react-redux'; 
+import { StateProvider } from './state/state'; 
 import Navbar from "./nav/navbar";
 import MainPage from "./main/main_page";
 import SessionContainer from './session/session_container';
@@ -16,6 +17,24 @@ const App = () => {
   const { isShowing, toggle } = useModal();
   const loggedIn = useSelector((state) => state.session.isAuthenticated);
   const history = useHistory(); 
+  const initialState = {};
+  for(let i = 0; i < 20; i++) initialState[i + 1] = {}; 
+  const reducer = (state, action) => {
+    let nextState = { ...state };
+
+    switch(action.type) {
+      case 'addGridItems':
+        action.gridItems.forEach(gridItem => {
+          let row = Object.keys(gridItem)[0]; 
+          let col = (Object.values(gridItem))[0][0]; 
+          let val = (Object.values(gridItem))[0][1]; 
+          nextState[row][col] = val;    
+        });
+        return nextState;
+      default: 
+        return state; 
+    }
+  }
 
   useEffect(() => {
     if (loggedIn) {
@@ -31,18 +50,20 @@ const App = () => {
     height: 100%; 
     position: relative; 
   `
- 
+ //{row : [col, val]}
   return (
-    <AppContainer>
-      <Navbar sticky={stick} ele={ele}/>
-      <Switch>
-        <ProtectedRoute exact path="/" component={MainPage}/>
-        <ProtectedRoute exact path="/newGame" component={MainPage}/>
-        <AuthRoute exact path="/login" component={SessionContainer} isShowing={isShowing}/>
-        <AuthRoute exact path="/signup" component={SessionContainer} isShowing={isShowing}/>
-      </Switch>
-      <Footer loggedIn={loggedIn}/>
-    </AppContainer>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <AppContainer>
+        <Navbar sticky={stick} ele={ele}/>
+        <Switch>
+          <ProtectedRoute exact path="/" component={MainPage}/>
+          <ProtectedRoute exact path="/newGame" component={MainPage}/>
+          <AuthRoute exact path="/login" component={SessionContainer} isShowing={isShowing}/>
+          <AuthRoute exact path="/signup" component={SessionContainer} isShowing={isShowing}/>
+        </Switch>
+        <Footer loggedIn={loggedIn}/>
+      </AppContainer>
+    </StateProvider>
   )
 };
 
