974bdb7aaa578af4624477074f440c0087a14163
Jacob <prall.jacob@gmail.com>
2020-09-03 10:28:56 -0700

    format

--- routes/api/route_helpers/user/stats.js
+++ routes/api/route_helpers/user/stats.js
@@ -4,38 +4,34 @@ const User = require('../../../../models/User');
 const Game = require('../../../../models/Game');
 const game = require('../game');
 
-
 module.exports = (req, res) => {
-    const { params: {id}, body } = req;
+  const {
+    params: { id },
+    body,
+  } = req;
 
-    User.findById(id)
-        .then(
-            user => {
-                const gameIds = user.games;
-                Game.find({ user: id })
-                    .then (
-                        games => {
-                            const gamesArr = [];
-                            let avgScore = 0;
-                            let avgWordsSent = 0;
-                            let avgGameTime = 0;
-                            for (let i = 0; i < games.length; i++) {
-                                gamesArr.push(games[i].id);
-                                avgScore += games[i].score;
-                                avgWordsSent += 1;
-                                avgGameTime += games[i].timeElapsed;
-                            }
-                            avgScore = (avgScore / games.length);
-                            avgWordsSent = (avgWordsSent / games.length);
-                            avgGameTime = (avgGameTime / games.length);
-                            res.json({
-                                gamesArr,
-                                avgScore,
-                                avgWordsSent,
-                                avgGameTime,
-                            })
-                        }
-                    )
-            }
-        )
+  User.findById(id).then((user) => {
+    const gameIds = user.games;
+    Game.find({ user: id }).then((games) => {
+      const gamesArr = [];
+      let avgScore = 0;
+      let avgWordsSent = 0;
+      let avgGameTime = 0;
+      for (let i = 0; i < games.length; i++) {
+        gamesArr.push(games[i].id);
+        avgScore += games[i].score;
+        avgWordsSent += 1;
+        avgGameTime += games[i].timeElapsed;
+      }
+      avgScore = avgScore / games.length;
+      avgWordsSent = avgWordsSent / games.length;
+      avgGameTime = avgGameTime / games.length;
+      res.json({
+        gamesArr,
+        avgScore,
+        avgWordsSent,
+        avgGameTime,
+      });
+    });
+  });
 };
