10c27de3e943a023143e9a951e55720b3ffe6819
timharding <timharding31@gmail.com>
2020-09-02 19:27:20 -0700

    Create game actions

---
+++ frontend/src/actions/game_actions.js
@@ -0,0 +1,20 @@
+import { getNewGame, patchGame } from '../util/game_util';
+
+export const RECEIVE_GAME = 'RECEIVE_GAME';
+export const RECEIVE_GAME_DETAILS = 'RECEIVE_NEXT_CLUE';
+
+const receiveGame = game => ({
+  type: RECEIVE_GAME,
+  game
+});
+
+const receiveGameDetails = gameDetails => ({
+  type: RECEIVE_GAME_DETAILS,
+  gameDetails
+});
+
+export const fetchNewGame = () => dispatch => getNewGame()
+  .then(game => dispatch(receiveGame(game)));
+
+export const updateGameDetails = gameUpdates => dispatch => patchGame(gameUpdates)
+  .then(gameDetails => dispatch(receiveGameDetails(gameDetails)));
