7e099e32ab5cb5969e7676f48e2bf2ddd6b8dc62
Phil Gresh <46543327+philgresh@users.noreply.github.com>
2020-09-03 10:26:24 -0700

    Update game_util.js

--- frontend/src/util/game_util.js
+++ frontend/src/util/game_util.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export const getNewGame = () => {
-  const token = JSON.parse(localStorage.getItem('jwtToken'));
+  const token = localStorage.getItem('jwtToken');
   axios.defaults.headers.common['Authorization'] = token;
   return axios.get('/api/games/new');
 }
 
 export const patchGame = ({ gameId, guess, timeRemaining, timeElapsed }) => {
-  const token = JSON.parse(localStorage.getItem('jwtToken'));
+  const token = localStorage.getItem('jwtToken');
   axios.defaults.headers.common['Authorization'] =  token;
   return axios.patch(`api/games/${gameId}`, { guess, timeRemaining, timeElapsed });
 }
