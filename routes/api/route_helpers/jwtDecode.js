c7ead0940c72da1af1797254dc3966cc799a5f14
Phil Gresham <philgresham@gmail.com>
2020-09-02 17:16:59 -0700

    minor change to jwtDecode

--- routes/api/route_helpers/jwtDecode.js
+++ routes/api/route_helpers/jwtDecode.js
@@ -1,10 +1,15 @@
 const jwt = require('jsonwebtoken');
 const { secretOrKey } = require('../../../config/keys');
 
+/**
+ * Verifies and decodes a JWT token
+ * @param {String} token JWT token sent in with authorization header
+ * @param {Object} res Express RES object
+ */
 const jwtDecode = (token, res) =>
   jwt.verify(token, secretOrKey, (err, decoded) => {
     if (err) {
-      return res.status(400).json(err);
+      return res.status(401).json(err);
     }
     return decoded;
   });
