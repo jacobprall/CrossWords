0ea6845b7e4767bee7a97ce2d17284154d3a21e8
Phil Gresham <philgresham@gmail.com>
2020-09-02 23:28:51 -0700

    Fix error

--- routes/api/route_helpers/game/newGame/utils.js
+++ routes/api/route_helpers/game/newGame/utils.js
@@ -3,15 +3,17 @@ const Word = require('../../../../../models/Word');
 const randNum = () => Math.floor(Math.random() * 26);
 const randLetter = () => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[randNum()];
 
-
 /**
- * 
+ * Generates a new word at random (26^3 chance of collision)
  * @returns {Word} - Word object, Mongoose model
  */
-const getFirstWord = () =>
-  Word.findOne({
-    answer: { $gte: `${randLetter}${randLetter}${randLetter}` },
+const getFirstWord = () => {
+  const randLetterSequence = `${randLetter()}${randLetter()}${randLetter()}`;
+  return Word.findOne({
+    difficulty: { $lte: 2 },
+    answer: { $gte: randLetterSequence },
   });
+};
 
 module.exports = {
   getFirstWord,
