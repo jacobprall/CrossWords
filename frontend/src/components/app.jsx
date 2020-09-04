638bdb1cea8032c8a082a328227d282bf5ffc0ff
Phil Gresham <philgresham@gmail.com>
2020-09-04 14:28:35 -0700

    Add normalize.css, adjust grid, styling

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,56 +1,73 @@
-import React, { useState, useEffect } from "react";
-import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch, useHistory } from "react-router-dom";
-import { useSelector } from 'react-redux'; 
-import { StateProvider } from './state/state'; 
-import Navbar from "./nav/navbar";
-import MainPage from "./main/main_page";
+import React, { useState, useEffect } from 'react';
+import { Switch, useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { StateProvider } from './state/state';
+import Navbar from './nav/navbar';
+import MainPage from './main/main_page';
 import SessionContainer from './session/session_container';
 import { StatsPage } from './stats/stats';
 import { mainReducer, initialState } from './react_reducers/main_reducer';
-import { Footer } from './footer/footer'; 
+import { Footer } from './footer/footer';
 import { useModal } from './custom_hooks/useModal';
-import useStick from './custom_hooks/useStick'; 
-import styled from 'styled-components'; 
+import useStick from './custom_hooks/useStick';
+import 'normalize.css';
 
 const App = () => {
   const { stick, ele } = useStick();
   const { isShowing, toggle } = useModal();
-  const [secondsElapsed, setSecondsElapsed] = useState(); 
+  const [secondsElapsed, setSecondsElapsed] = useState();
   const loggedIn = useSelector((state) => state.session.isAuthenticated);
-  const history = useHistory(); 
-  
+  const history = useHistory();
+
   useEffect(() => {
     if (loggedIn) {
       toggle(false);
-      history.replace("/");
+      history.replace('/');
     } else {
-      toggle(true); 
+      toggle(true);
     }
-  }, [loggedIn])
+  }, [loggedIn]);
 
   const endGame = () => {
-    toggle(true); 
-  }
+    toggle(true);
+  };
 
   const AppContainer = styled.div`
-    height: 100%; 
-    position: relative; 
-  `
+    height: 100%;
+    position: relative;
+  `;
   return (
     <StateProvider initialState={initialState} reducer={mainReducer}>
       <AppContainer>
-        <Navbar sticky={stick} ele={ele}/>
+        <Navbar sticky={stick} ele={ele} />
         <Switch>
-          <ProtectedRoute exact path="/newGame" component={MainPage} endGame={endGame} isShowing={isShowing}/>
-          <ProtectedRoute exact path="/stats" component={StatsPage}/>
-          <AuthRoute exact path="/login" component={SessionContainer} isShowing={isShowing}/>
-          <AuthRoute exact path="/signup" component={SessionContainer} isShowing={isShowing}/>
+          <ProtectedRoute
+            exact
+            path="/newGame"
+            component={MainPage}
+            endGame={endGame}
+            isShowing={isShowing}
+          />
+          <ProtectedRoute exact path="/stats" component={StatsPage} />
+          <AuthRoute
+            exact
+            path="/login"
+            component={SessionContainer}
+            isShowing={isShowing}
+          />
+          <AuthRoute
+            exact
+            path="/signup"
+            component={SessionContainer}
+            isShowing={isShowing}
+          />
         </Switch>
-        <Footer loggedIn={loggedIn}/>
+        <Footer loggedIn={loggedIn} />
       </AppContainer>
     </StateProvider>
-  )
+  );
 };
 
 export default App;
