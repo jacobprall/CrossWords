be6ed2a05ae7d3a8bf7f3fad03ba0adb15d38155
timharding <timharding31@gmail.com>
2020-09-07 17:40:06 -0700

    Test & style Reveal button

--- frontend/src/components/time_score_clue/clue.jsx
+++ frontend/src/components/time_score_clue/clue.jsx
@@ -5,7 +5,12 @@ import { getClueAnswer } from '../../util/word_util';
 import { useStateValue } from '../state/state'; 
 import { useEffect } from 'react';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Header = styled.div`
   display: flex;
@@ -23,6 +28,18 @@ const YourClue = styled.h4`
   font-weight: 500;
   overflow-wrap: normal;
   word-wrap: break-word;
+`;
+
+const RevealButton = styled.button`
+  width: 15%;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+  background-color: transparent;
+  color: #101010;
+  outline: none;
+  border: 1px solid #4e89ae;
+  font-weight: 300;
+  border-radius: 0.2rem;
 `;
 
 export const Clue = ({ clue }) => {
@@ -59,8 +76,8 @@ export const Clue = ({ clue }) => {
         <MagnifyingGlass src={magnifying_glass} />
         <YourClue>{`${clue.clue} (${clue.length} letters)`}</YourClue>
       </Header>
+      <RevealButton onClick={handleReveal}>Reveal</RevealButton>
       {/* {clue._id ? null : <button onClick={handleReveal}>Reveal</button>} */}
-      <button onClick={handleReveal}>Reveal</button>
     </Container>
   );
 };
